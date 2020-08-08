f079e120f49a7f484b44865fb240fe47781486bd
Neilson <neoviera86@gmail.com>
2020-08-08 16:58:04 -0300

    Alteração de Projeto 08/08/2020

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import Home from './pages/Home';
+import CadastroVideo from './pages/cadastro/Video';
+
+
+const Pagina404 = () => (<div> Página 404 </div>)
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+      <Route path="/cadastro/video"component={CadastroVideo} />
+      <Route path="/"component={Home} exact/>
+      <Route component={Pagina404} />
+    </Switch>
+  </BrowserRouter>,
+  //<React.StrictMode>
+  // <App />
+  //</React.StrictMode>,
   document.getElementById('root')
 );
 
